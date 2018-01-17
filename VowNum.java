bf32c9d3e4bf4ca09cb77ed2eb398ae2c4434b98
Sowmiya11 <sowmiyapriya1112@gmail.com>
2018-01-17 17:26:03 +0530

    Add files via upload

---
+++ VowNum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class VowNum
+{
+  Scanner scan=new Scanner(System.in);
+  char ch=scan.next();
+  if(ch<='a' && ch>='z' || ch>='A' && ch<='Z')
+  {
+    System.out.println("character");
+  }
+  else 
+   System.out.println("number");
+}
