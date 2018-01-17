25a75c73f802148bf5b6c260bb3e732609b841a3
Sowmiya11 <sowmiyapriya1112@gmail.com>
2018-01-17 18:10:00 +0530

    Add files via upload

---
+++ Nat.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Nat
+{
+  Scanner scan=new Scanner(System.in);
+  System.out.println("Enter the number");
+  int num=scan.next();
+ int sum=0;
+ for(int i=0;i<=sum;i++)
+   sum+=i;
+  System.out.println(sum);
+scan.close();
+}
