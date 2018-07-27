1c719d94629c62917892471b45d28bb14082ce1b
Sowmiya11 <sowmiyapriya1112@gmail.com>
2018-07-27 13:50:07 +0530

    Update CountNumericChar.java

--- Begineer/Set4/CountNumericChar.java
+++ Begineer/Set4/CountNumericChar.java
@@ -5,7 +5,7 @@ public class CountNumericChar
 	public static void main(String[] args) 
 	{
 		Scanner s=new Scanner(System.in);
-		String str=s.next();
+		String str=s.nextLine();
 		int count=0;
 		for(int i=0;i<str.length();i++)
 		{
