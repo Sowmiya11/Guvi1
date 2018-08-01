cd681eb92723159b004d60e52db65944dc06a593
Sowmiya11 <sowmiyapriya1112@gmail.com>
2018-08-01 14:15:36 +0530

    Add files via upload

---
+++ Player/Set2/MostRepLetter.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class MostRepLetter
+{
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		String str=s.nextLine();
+		if(str.length()>=1&&str.length()<=100000)
+		{
+			int c=0,max=0;
+			char ch[]=str.toCharArray(),cha='\u0000';
+			for(int i=0;i<ch.length;i++)
+			{
+				c=1;
+				for(int j=i;j<ch.length;j++)
+				{
+					if(ch[i]==ch[j])
+					{
+						c++;
+					}
+				}
+				if(c>max)
+				{
+					max=c;
+					cha=ch[i];
+				}
+			}
+			System.out.println(cha);
+		}
+		s.close();
+	}
+}
