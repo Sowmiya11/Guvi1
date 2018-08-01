b9885c10afb6b0dc74c8dbc5596c809e8fc37597
Sowmiya11 <sowmiyapriya1112@gmail.com>
2018-08-01 13:55:46 +0530

    Add files via upload

---
+++ Player/Set2/SquareProductOfDigits.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SquareProductOfDigits 
+{
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int digit=0;
+		while(n>0)
+		{
+			digit+=((n%10)*(n%10));
+			n/=10;
+		}
+		System.out.println(digit);
+		s.close();
+	}
+}
